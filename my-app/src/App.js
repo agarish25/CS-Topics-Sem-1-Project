078c9fb2763ed8f50e40b74d9762083a57869b3d
Ishaan Agarwal <agarwal.m.ishaan@gmail.com>
2025-01-05 16:57:52 -0800

    react app changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home";
+import GamePage from "./Pages/GamePage";
+import About from "./Pages/About";
+import Leaderboard from "./Pages/About";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game/:id" element={<GamePage />} />
+        <Route path="about" element={<About />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+        
+    </Router>
   );
 }
 
